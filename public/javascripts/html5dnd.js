11ae61b7205ad530b3d4fb734e5318965e532e62
Alex Cotoranu <alexcotoranu@gmail.com>
2015-06-09 17:19:30 -0700

    v0.0.3
    You can now make groups of two cards and name them... just about
    anything else breaks.
    
    You can still shuffle them around in the sidebar.
    
    Added a random colour to the groups.
    
    Played around with the style a bit.

--- public/javascripts/html5dnd.js
+++ public/javascripts/html5dnd.js
@@ -16,6 +16,32 @@ Element.prototype.removeClassName = function(name) {
   }
 };
 
+//function to check for class
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+//function to create a GUID
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+//function to generate random HEX colour
+function randomHexColour() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+//function to prevent enter key from submitting forms
+function noenter() {
+  return !(window.event && window.event.keyCode == 13); 
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     var id_ = 'cards';
     var cards_ = document.querySelectorAll('#' + id_ + ' .card');
@@ -58,23 +84,105 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         // Don't do anything if we're dropping on the same card we're dragging.
         if (dragSrcEl_ != this) {
-            // Check if cards are being dropped in sorting space
-            if (sortSpace_ == this){
+            
+            if ( sortSpace_ == this && ( hasClass(dragSrcEl_,'card-sortable') == false ) ) {
+                //if cards are being dropped in sorting space
+                dragSrcEl_.addClassName('-sortable');
                 sortSpace_.appendChild(dragSrcEl_);
-            // Otherwise shuffle as usual
-            } else {
+
+            } else if ( hasClass(this,'card-sortable') && hasClass(dragSrcEl_,'card-sortable') ) {
+                //if a sortable card is being dropped onto another sortable card
+                console.log("Both target and source are sortable");
+
+                // if target is in a group
+                if (hasClass(this.parentNode,'.group')) {
+                    console.log("The target is in a group.");  
+                    // if source is in a group as well
+                    if (hasClass(dragSrcEl_.parentNode,'.group')) {
+                        console.log("The source is in a group as well.");
+                        // if target and source are both in the same group
+                        if(this.parentNode.id == dragSrcEl_.parentNode.id) {
+                            console.log("In fact, they are in the same group.");
+                            // first clean up extra classes
+                            dragSrcEl_.removeClassName('moving');
+                            this.removeClassName('over');
+                            // then make another group inside that group
+                            var newGuid = guid();
+                            var newColour = randomHexColour();
+                            var groupContent = this.parentNode.innerHTML;
+                            this.parentNode.innerHTML = '<div class="group" id="' + newGuid + '" style="border-color:' + newColour + ';"><form><input type="text" name="' + newGuid + '_input" placeholder="Group Name Here..."></input></form>' + groupContent + '</div>';
+                        } else { // if the source is in another group
+                            console.log("However, they are in different groups.");
+                            // first clean up extra classes
+                            dragSrcEl_.removeClassName('moving');
+                            this.removeClassName('over');
+                            // then add it to the target's group
+                            var groupContent = this.parentNode.innerHTML + dragSrcEl_.outerHTML;
+                            this.parentNode.innerHTML = groupContent;
+                            // then remove the source from it's original group
+                            dragSrcEl_.outerHTML=null;
+                        }
+                    } else { // if the source is not in a group
+                        console.log("But the source is not in a group.");
+                        // first clean up extra classes
+                        dragSrcEl_.removeClassName('moving');
+                        this.removeClassName('over');
+                        // then add it to the target's group
+                        var groupContent = this.parentNode.innerHTML + dragSrcEl_.outerHTML;
+                        this.parentNode.innerHTML = groupContent;
+                        // then remove the source from the sorting area or deck
+                        dragSrcEl_.outerHTML=null;
+                    }
+ 
+                } else { // else if target is not in a group
+                    console.log("The target is not in a group.");
+                    // if the source is in a group
+                    if (hasClass(dragSrcEl_.parentNode,'.group')) {
+                        console.log("The source is in a group");
+
+                        // then create a new group and add both the target and source to it
+                        // then remove the source from it's original group and the target from the deck
+                    } else { // if the source is not in a group
+                        console.log("The source is not in a group.");
+                        // first clean up extra classes
+                        dragSrcEl_.removeClassName('moving');
+                        this.removeClassName('over');
+                        // then replace the target with new group which contains both the target and source
+                        var newGuid = guid();
+                        var newColour = randomHexColour();
+                        var groupContent = this.outerHTML + dragSrcEl_.outerHTML;
+                        this.outerHTML = '<div class="group" id="' + newGuid + '"style="border-color:' + newColour + '"><form><input type="text" placeholder="Group Name Here..."></input></form>' + groupContent + '</div>';
+                        // then remove the source from the deck or sorting area
+                        dragSrcEl_.outerHTML = null;
+                    }
+                }
+
+            } else if ( hasClass(this,'card-sortable') && !hasClass(dragSrcEl_,'card-sortable') ) {
+                //do nothing if only the target card is sortable
+                console.log("Only target is sortable");
+            } else if ( !hasClass(this,'card-sortable') && hasClass(dragSrcEl_,'card-sortable') ) {
+                //if only the source card is sortable
+                console.log("Only source is sortable");
+                // then add the source to the target (sorting area)
+                sortSpace_.appendChild(dragSrcEl_);
+                // then remove the source from its group
+                //dragSrcEl_.innerHTML=null;
+            } else { // Otherwise switch cards as usual
+                console.log("Swapping cards.");
                 dragSrcEl_.innerHTML = this.innerHTML;
                 this.innerHTML = e.dataTransfer.getData('text/html');
             }
 
+            console.log(this);
+            console.log(dragSrcEl_);
+
             // Set number of times the card has been moved.
             var count = this.querySelector('.count');
 
-            // var newCount = parseInt(count.getAttribute('data-card-moves')) + 1;
-            var dataCardMoves = parseInt(count.getAttribute('data-card-moves')) || 0;
-            var newCount = dataCardMoves + 1;
-            count.setAttribute('data-card-moves', newCount);
-            count.textContent = 'moves: ' + newCount;  
+            // var dataCardMoves = parseInt(count.getAttribute('data-card-moves')) || 0;
+            // var newCount = dataCardMoves + 1;
+            // count.setAttribute('data-card-moves', newCount);
+            // count.textContent = 'moves: ' + newCount;  
         }
 
         return false;
@@ -100,21 +208,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
       card.addEventListener('dragend', handleDragEnd, false);
     });
 
-    var cardsBeingSorted = document.querySelectorAll('#sorting-space .card');
-    [].forEach.call(cardsBeingSorted, function(card) {
-      card.addEventListener('dragstart', handleDragStart, false);
-      card.addEventListener('dragenter', handleDragEnter, false);
-      card.addEventListener('dragover', handleDragOver, false);
-      card.addEventListener('dragleave', handleDragLeave, false);
-      card.addEventListener('drop', handleDrop, false);
-      card.addEventListener('dragend', handleDragEnd, false);
-    });
 
-    sortSpace_.addEventListener('dragstart', handleDragStart, false);
+
+    // var cardsBeingSorted = document.querySelectorAll('#sorting-space .card');
+    // [].forEach.call(cardsBeingSorted, function(card) {
+    //   card.removeEventListener('dragstart', handleDragStart, false);
+    //   card.removeEventListener('dragenter', handleDragEnter, false);
+    //   card.removeEventListener('dragover', handleDragOver, false);
+    //   card.removeEventListener('dragleave', handleDragLeave, false);
+    //   card.removeEventListener('drop', handleDrop, false);
+    //   card.removeEventListener('dragend', handleDragEnd, false);
+    // });
+
+    // sortSpace_.addEventListener('dragstart', handleDragStart, false);
     sortSpace_.addEventListener('dragenter', handleDragEnter, false);
     sortSpace_.addEventListener('dragover', handleDragOver, false);
     sortSpace_.addEventListener('dragleave', handleDragLeave, false);
     sortSpace_.addEventListener('drop', handleDrop, false);
-    sortSpace_.addEventListener('dragend', handleDragEnd, false);
+    // sortSpace_.addEventListener('dragend', handleDragEnd, false);
 
 });
